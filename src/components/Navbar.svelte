e7678ee774b7ff0eebb0efa1f3d967b9b310e2cd
Luis Alejandro <luisalejandromedinab@gmail.com>
2023-07-17 17:49:54 -0500

    Fin Bootstrap Navbar

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -14,9 +14,31 @@
     console.log("Si");
     navigate("/login", { replace: true });
   };
+
+   const cerrarSesion = async () => {
+    try {
+      await signOut(auth);
+      user.setUser(null);
+      navigate("/login", { replace: true });
+      console.log("se ha cerrado cesión con exito");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 </script>
 
 <!--Links para las vistas de todo el publico -->
 <Link to="/">Inicio</Link>
 <Link to="/login">Login</Link>
-<Link to="/perfil">Perfil</Link>
+{#if $user}
+   <!-- content here -->
+   <Link to="/perfil">Perfil</Link>
+{/if}
+
+<!--Create a buuton with bootstrap danger-->
+{#if !$user}
+   <button class="btn btn-warning" on:click={goLogin}>Iniciar Sesión</button>
+{:else}
+   <button class="btn btn-danger" on:click={cerrarSesion}>Cerrar Sesión</button>
+{/if}
+
