159c147f304f990a8bccb8d65f0c4e76e817cb16
Jesus G <jesusdavidgd200425@gmail.com>
2023-08-11 12:40:12 -0500

    Restaurantes estilos

--- src/views/Restaurante.svelte
+++ src/views/Restaurante.svelte
@@ -77,171 +77,76 @@
 
 </script>
 
-<h1>Restaurantes</h1>
+<h1 style="color: #1687ED;">Restaurantes</h1>
 
 <!--Busqueda de restaurantes-->
-<input type="text" placeholder="Busque por el nombre del restaurtante" bind:value={busqueda} >
+<input type="text" placeholder="Busque por el nombre del restaurante" bind:value={busqueda} style="margin-bottom: 20px; padding: 5px; border: 1px solid #1687ED; border-radius: 5px;">
 
 <div class="container1">
   {#each restaurantes as restaurante}
     {#if restaurante.visible}
-      <div style="border: 2px solid; margin:7px ;">
+      <div class="restaurant-card">
         <h1>{restaurante.nombre}</h1>
         <h2>{restaurante.direccion}</h2>
         <h3>{restaurante.telefono}</h3>
         <h4>{restaurante.horario}</h4>
         <h5>{restaurante.descripcion}</h5>
-        <h6>{restaurante.visible}</h6>
-        <button on:click={() =>gotoMenu(restaurante)}>Ver Menu</button>
+        <button on:click={() => gotoMenu(restaurante)}>Ver Menú</button>
       </div>
     {/if}
   {/each}
-  </div>
+</div>
 
 <style>
   .container1 {
     display: flex;
-    flex-direction: row; /* Esto hace que los componentes se muestren en una fila horizontal */
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
   }
 
+  .restaurant-card {
+    border: 2px solid #1687ED;
+    padding: 15px;
+    border-radius: 10px;
+    width: 300px;
+    background-color: #fff;
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+  }
 
-  @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
-*{
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
-}
+  .restaurant-card:hover {
+    transform: translateY(-5px);
+    box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
+  }
 
-html,body{
-  display: grid;
-  height: 100%;
-  place-items: center;
-  text-align: center;
-  background: #000;
-}
+  .restaurant-card h1 {
+    font-size: 24px;
+    color: #1687ED;
+    margin-bottom: 5px;
+  }
 
-.container{
-  position: relative;
-  width: 400px;
-  background: #111;
-  padding: 20px 30px;
-  border: 1px solid #444;
-  border-radius: 5px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-}
-.container .post{
-  display: none;
-}
-.container .text{
-  font-size: 25px;
-  color: #666;
-  font-weight: 500;
-}
-.container .edit{
-  position: absolute;
-  right: 10px;
-  top: 5px;
-  font-size: 16px;
-  color: #666;
-  font-weight: 500;
-  cursor: pointer;
-}
-.container .edit:hover{
-  text-decoration: underline;
-}
-.container .star-widget input{
-  display: none;
-}
-.star-widget label{
-  font-size: 40px;
-  color: #444;
-  padding: 10px;
-  float: right;
-  transition: all 0.2s ease;
-}
-input:not(:checked) ~ label:hover,
-input:not(:checked) ~ label:hover ~ label{
-  color: #fd4;
-}
-input:checked ~ label{
-  color: #fd4;
-}
-input#rate-5:checked ~ label{
-  color: #fe7;
-  text-shadow: 0 0 20px #952;
-}
-#rate-1:checked ~ form header:before{
-  content: "I just hate it ";
-}
-#rate-2:checked ~ form header:before{
-  content: "I don't like it ";
-}
-#rate-3:checked ~ form header:before{
-  content: "It is awesome ";
-}
-#rate-4:checked ~ form header:before{
-  content: "I just like it ";
-}
-#rate-5:checked ~ form header:before{
-  content: "I just love it ";
-}
-.container form{
-  display: none;
-}
-input:checked ~ form{
-  display: block;
-}
-form header{
-  width: 100%;
-  font-size: 25px;
-  color: #fe7;
-  font-weight: 500;
-  margin: 5px 0 20px 0;
-  text-align: center;
-  transition: all 0.2s ease;
-}
-form .textarea{
-  height: 100px;
-  width: 100%;
-  overflow: hidden;
-}
-form .textarea textarea{
-  height: 100%;
-  width: 100%;
-  outline: none;
-  color: #eee;
-  border: 1px solid #333;
-  background: #222;
-  padding: 10px;
-  font-size: 17px;
-  resize: none;
-}
-.textarea textarea:focus{
-  border-color: #444;
-}
-form .btn{
-  height: 45px;
-  width: 100%;
-  margin: 15px 0;
-}
-form .btn button{
-  height: 100%;
-  width: 100%;
-  border: 1px solid #444;
-  outline: none;
-  background: #222;
-  color: #999;
-  font-size: 17px;
-  font-weight: 500;
-  text-transform: uppercase;
-  cursor: pointer;
-  transition: all 0.3s ease;
-}
-form .btn button:hover{
-  background: #1b1b1b;
-}
+  .restaurant-card h2,
+  .restaurant-card h3,
+  .restaurant-card h4,
+  .restaurant-card h5 {
+    font-size: 14px;
+    color: #666;
+    margin-bottom: 3px;
+  }
+
+  .restaurant-card button {
+    background-color: #1687ED;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 5px;
+    font-size: 14px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  .restaurant-card button:hover {
+    background-color: #1369c2;
+  }
 </style>
