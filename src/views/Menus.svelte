493e265fc102886234038ba1af7ca61f08fd8859
Luis Alejandro <luisalejandromedinab@gmail.com>
2023-08-11 11:33:58 -0500

    parte calificación

--- src/views/Menus.svelte
+++ src/views/Menus.svelte
@@ -1,21 +1,21 @@
 <script>
-//Nos suscribimos a las stores
-import { menuStore, getMenu } from "../stores/Menu";
+  //Nos suscribimos a las stores
+  import { menuStore, getMenu } from "../stores/Menu";
 
-//importamos onMount para que se ejecute cuando se monte el componente
-import { onDestroy, onMount } from "svelte";
+  //importamos onMount para que se ejecute cuando se monte el componente
+  import { onDestroy, onMount } from "svelte";
 
-//importamos lo necesario para comunicar con la base de datos
-import { db } from "../firebase";
-import {
-  addDoc,
-  collection,
-  onSnapshot,
-  updateDoc,
-  doc,
-} from "firebase/firestore";
+  //importamos lo necesario para comunicar con la base de datos
+  import { db } from "../firebase";
+  import {
+    addDoc,
+    collection,
+    onSnapshot,
+    updateDoc,
+    doc,
+  } from "firebase/firestore";
 
-let restaurante = {
+  let restaurante = {
     idRestaurante: 0,
     img: "https://firebasestorage.googleapis.com/v0/b/ocean-ad72b.appspot.com/o/Restaurantes%2Fnot-found-icon-4.jpg?alt=media&token=5b0a8e01-c4b6-4218-8983-bbc4648cbf67",
     nombre: "ejemplo",
@@ -28,57 +28,66 @@ let restaurante = {
     visible: true,
   };
 
-
-//traemos el menu guardado
-onMount(async () => {
-  //obtenemos el evento
-  console.log("este");
-  menuStore.subscribe((value) => {
-    restaurante = value;
-    console.log(restaurante.nombre);
-    if (restaurante == null) {
-      console.log("No hay evento");
-    }
-  });
-});
-
-//Traemos la base de datos de Menus
-//creamos una variable para guardar los menus
-let menus = [];
-
-const unsub = onSnapshot(
-  collection(db, "Menus"),
-  (querySnapshot) => {
-    menus = querySnapshot.docs.map((doc) => {
-      return { ...doc.data(), id: doc.id }; //con esto decimos que por cada recorrido trasformamos los datos en un objeto
+  //traemos el menu guardado
+  onMount(async () => {
+    //obtenemos el evento
+    console.log("este");
+    menuStore.subscribe((value) => {
+      restaurante = value;
+      console.log(restaurante.nombre);
+      if (restaurante == null) {
+        console.log("No hay evento");
+      }
     });
+  });
 
-    menus = menus.filter((menu) => {
+  //Traemos la base de datos de Menus
+  //creamos una variable para guardar los menus
+  let menus = [];
+
+  const unsub = onSnapshot(
+    collection(db, "Menus"),
+    (querySnapshot) => {
+      menus = querySnapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id }; //con esto decimos que por cada recorrido trasformamos los datos en un objeto
+      });
+
+      menus = menus.filter((menu) => {
         return menu.idRestaurante == restaurante.idRestaurante;
       });
 
-    console.log(menus);
-  },
-  (error) => {
-    console.log(error);
-  }
-);
+      console.log(menus);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
 
-onDestroy(() => {
-  unsub();
-});
+  onDestroy(() => {
+    unsub();
+  });
 
-
+  //Funciones para calaificar un menu
+  //Variable para saber cuando se esta calificando un menu
+  let calificando = false;
+  const mostrarMenuCalificacion = () => {
+    calificando = !calificando;
+  };
 </script>
 
 <h1>Menusss</h1>
 <h1>{restaurante.nombre}</h1>
 
 {#each menus as menu}
-    <h1>{menu.nombre}</h1>
+  <h1>{menu.nombre}</h1>
 {/each}
 
+{#if !calificando}
+  <button on:click={mostrarMenuCalificacion}>Calificar restaurante</button>
+{:else}
+  Aqui la parate pata calificar restaurante
+  <button on:click={mostrarMenuCalificacion}> Enviar calificación</button>
+{/if}
+
 <style>
-
-
 </style>
